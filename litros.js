f50d2ff3bb76b84ae0fd0dcd83ddb70d57003b1d
Michel Rechinelli <michel.tupi2@gmail.com>
2023-01-12 10:50:42 -0300

    cauculadora de lumens não funciona

---
+++ litros.js
@@ -0,0 +1,10 @@
+function mult(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = document.getElementById("n3").value;
+    var n4 = 1000;
+   
+    var m  = n1*n2*n3/n4; 
+
+    document.querySelector(".result").innerHTML = m;
+} 
