3f09cd899a746cbfa34d95acdf8654bca470a942
Michel Rechinelli <michel.tupi2@gmail.com>
2023-01-13 01:34:10 -0300

    js termostato
    criação das funções para o termostato

--- lumens.js
+++ lumens.js
@@ -5,14 +5,14 @@
                 var n4 = 20;
                
                 var m  = n1*n4; 
-
+            
                 var d = m/1000;
-
+            
                 var e = ' Lâmpadas LED de 10W';
                 var f = ' Lumén, isso equivale a:'
-
+            
                 document.querySelector(".result").innerHTML = m + f;
                 document.querySelector(".result2").innerHTML = d + e;
-            } 
+            }
          
            
---
+++ termostato.js
@@ -0,0 +1,17 @@
+function mult(){
+    var n1 = document.getElementById("n1").value;
+    
+    var n4 = 2;
+   
+    var m  = n1/n4; 
+
+    
+
+   
+    var f = ' Watts!'
+
+    document.querySelector(".result").innerHTML = m + f;
+ 
+} 
+
+ 
